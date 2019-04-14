6283ff6633b4326082159b50697c1c14ec90a28e
andriy kravets <4kravets@gmail.com>
2019-04-14 19:26:19 +0300

    adding homepage, trader model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :traders, only: [:create]
+  # resources :traders, except: [:show, :index, :new, :update, :destroy, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
