472666ade0eed09feb1de9375acc5627012cbd73
andriy kravets <4kravets@gmail.com>
2019-04-18 19:41:19 +0300

    fixes for heroku

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,8 @@ module Signalsapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
 end
