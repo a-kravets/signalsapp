b5c4c58ddd1864cf984ab9a782b382a012f9933a
andriy kravets <4kravets@gmail.com>
2019-04-18 20:55:01 +0300

    fixing...

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,13 +5,13 @@ const webpack = require('webpack')
 // Preventing Babel from transpiling NodeModules packages
 // environment.loaders.delete('nodeModules');
 
-// // Bootstrap 4 has a dependency over jQuery & Popper.js:
-// environment.plugins.prepend('Provide',
-//   new webpack.ProvidePlugin({
-//     $: 'jquery',
-//     jQuery: 'jquery',
-//     Popper: ['popper.js', 'default']
-//   })
-// )
+// Bootstrap 4 has a dependency over jQuery & Popper.js:
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
 
 module.exports = environment
